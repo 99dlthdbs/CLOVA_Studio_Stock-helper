0aabfc10317c73f37bcd9f58e2e75298e9d77316
videorighter <videorighter@gmail.com>
2024-05-30 08:46:30 +0900

    feat: crawler 예외 처리 수정

--- videorighter/news_crawler/002-003_news-crawler.py
+++ videorighter/news_crawler/002-003_news-crawler.py
@@ -1,12 +1,11 @@
 import requests
 from urllib.parse import urlparse, urlencode, urlunparse, quote_plus
-from datetime import datetime
+from datetime import datetime, timedelta
 from bs4 import BeautifulSoup as bs
 from pymongo import MongoClient
 import argparse
 import time
 import random
-
 
 def parse_args():
     parser = argparse.ArgumentParser(description="MongoDB connection parameters")
@@ -16,8 +15,8 @@ def parse_args():
     parser.add_argument('--password', type=str, default='financial', required=True, help='MongoDB password')
     parser.add_argument('--database', type=str, required=True, help='MongoDB database name')
     parser.add_argument('--query', type=str, required=True, help='검색 쿼리')
-    parser.add_argument('--ds', type=str, default=datetime.now().strftime("%Y.%m.%d.%H")+".00", help='시작 일시')
-    parser.add_argument('--de', type=str, default=datetime.now().strftime("%Y.%m.%d.%H.%M"), help='종료 일시')
+    parser.add_argument('--ds', type=str, help='시작 일시')
+    parser.add_argument('--de', type=str, help='종료 일시')
     return parser.parse_args()
 
 def make_url(base_url, params):
@@ -27,42 +26,43 @@ def make_url(base_url, params):
 
 def fetch_news_data(params, headers, url, db):
     flag = 0
-    while flag < 30:
+    while flag < 50:
+        print(flag)
         naver_news_links = []
-        batch = []        
+        batch = []
 
         new_url = make_url(url, params)
         response = requests.get(new_url, headers=headers)
         if response.status_code != 200:
             print("Connection Issue")
-            time.sleep(float(random.uniform(0.1, 0.5)))
+            time.sleep(float(random.uniform(0.1, 0.3)))
             continue
-        
+
         soup = bs(response.content, 'html.parser')
         naver_news_links = [a['href'][2:-2] for a in soup.find_all('a', string='네이버뉴스') if a['href']]
 
         if not naver_news_links:
             params['start'] = str(int(params['start']) + 10)
             print("No NAVER NEWS Platforms")
-            time.sleep(float(random.uniform(0.1, 0.5)))
+            time.sleep(float(random.uniform(0.1, 0.3)))
             flag += 1
             continue
-        
+
         for link in naver_news_links:
             print("link: ", link)
-            
+
             if 'sports' in link or 'entertain' in link:
                 continue
 
             article_res = requests.get(link)
-            
+
             if article_res.status_code != 200:
                 print("Connection Issue")
-                time.sleep(float(random.uniform(0.1, 0.5)))
+                time.sleep(float(random.uniform(0.1, 0.3)))
                 continue
-            
+
             article_soup = bs(article_res.content, 'html.parser')
-            
+
             try:
                 title = article_soup.select_one('#title_area > span').get_text(strip=True)
             except:
@@ -73,11 +73,11 @@ def fetch_news_data(params, headers, url, db):
 
             if params['query'] not in title:
                 print("Query is not in the Title")
-                time.sleep(float(random.uniform(0.1, 0.5)))
+                time.sleep(float(random.uniform(0.1, 0.3)))
                 continue
-            
+
             print("==========================================")
-            
+
             print("title: ", title)
             press_select = article_soup.select_one('#ct > div.media_end_head.go_trans > div.media_end_head_top._LAZY_LOADING_WRAP > a > img.media_end_head_top_logo_img.light_type._LAZY_LOADING._LAZY_LOADING_INIT_HIDE')
             press = press_select['title'] if 'title' in press_select.attrs else 'Title attribute not found'
@@ -87,18 +87,18 @@ def fetch_news_data(params, headers, url, db):
             print("date: ", date)
             summary = article_soup.select_one('#dic_area > strong').get_text(strip=True) if article_soup.select_one('#dic_area > strong') else None
             print("summary: ", summary)
-            
+
             for span in article_soup.find_all('span', class_='end_photo_org'):
                 span.decompose()
-            
+
             content = article_soup.select_one('#dic_area').get_text(strip=True).replace("\n\n\n", "")
-            
+
             if summary is not None:
                 index = content.find(summary)
                 content = content[index + len(summary):] if index != -1 else content
 
             print("content: ", content)
-            
+
             print("==========================================")
             tmp = {
                 'news_date': date.split(" ")[0],
@@ -111,58 +111,63 @@ def fetch_news_data(params, headers, url, db):
                 'url': link,
             }
             batch.append(tmp)
-            time.sleep(float(random.uniform(0.1, 0.5)))
+            time.sleep(float(random.uniform(0.1, 0.3)))
 
-        if batch != []:
-            tmp = 0
-            for news in batch:
-                existing_news = db.news.find_one({"url": news['url'], "query": news['query'], "news_date": news['news_date'], "news_time": news['news_time']})
-                
-                if not existing_news:
-                    db.news.insert_one(news)
-                    flag = 0
-                else:
-                    print("Duplication Issue")
-                    tmp += 1
-                    
-            if tmp == len(batch):
+        if batch:
+            urls = [news['url'] for news in batch]
+            existing_news = db.news.find({"url": {"$in": urls}, "query": params['query']})
+            existing_urls = {news['url'] for news in existing_news}
+
+            new_batch = [news for news in batch if news['url'] not in existing_urls]
+
+            if new_batch:
+                db.news.insert_many(new_batch)
+                flag = 0
+            else:
                 print("All batch data is Duplicated")
+                flag += 1
 
         print("start: ", params['start'])
         params['start'] = str(int(params['start']) + 10)
-        time.sleep(float(random.uniform(0.7, 1)))
+        time.sleep(float(random.uniform(0.4, 0.6)))
 
 def main(args):
+    start_date = datetime.strptime(args.ds, "%Y.%m.%d")
+    end_date = datetime.strptime(args.de, "%Y.%m.%d") + timedelta(days=1)
 
-    params = {
-        'filed': '0',
-        'is_dts': '0',
-        'is_sug_officeid': '0',
-        'office_category': '0',
-        'office_section_code': '0',
-        'service_area': '0',
-        'query': args.query,
-        'sort': '1',
-        'start': '1',
-        'where': 'news_tab_api',
-        # 'nso': 'so:dd,p:all,a:all',
-        'nso': f'so:dd,p:from{args.ds.replace(".", "")}to{args.de.replace(".", "")}',
-        # 'pd': '0',
-        'ds': args.ds,
-        'de': args.de
-    }
-    url = 'https://s.search.naver.com/p/newssearch/search.naver'
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
     }
 
     if args.port == '':
-        client = MongoClient(args.host, username=args.username, password=args.password)    
+        client = MongoClient(args.host, username=args.username, password=args.password)
     else:
         client = MongoClient(args.host, args.port, username=args.username, password=args.password)
-    
+
     db = client[args.database]
-    fetch_news_data(params, headers, url, db)
+
+    current_date = start_date
+    while current_date < end_date:
+        next_date = current_date + timedelta(days=1)
+        params = {
+            'filed': '0',
+            'is_dts': '0',
+            'is_sug_officeid': '0',
+            'office_category': '0',
+            'office_section_code': '0',
+            'service_area': '0',
+            'query': args.query,
+            'sort': '1',
+            'start': '1',
+            'where': 'news_tab_api',
+            'nso': f'so:dd,p:from{current_date.strftime("%Y%m%d")}to{current_date.strftime("%Y%m%d")}',
+            'ds': current_date.strftime("%Y.%m.%d"),
+            'de': current_date.strftime("%Y.%m.%d")
+        }
+
+        url = 'https://s.search.naver.com/p/newssearch/search.naver'
+        fetch_news_data(params, headers, url, db)
+        current_date = next_date
 
 if __name__ == "__main__":
     args = parse_args()
--- videorighter/news_crawler/002-003_news-crawler.sh
+++ videorighter/news_crawler/002-003_news-crawler.sh
@@ -1,8 +1,8 @@
-python 002-002_news-crawler.py \
+python 002-003_news-crawler.py \
     --host mongo.stockhelper-mongodb.store \
     --username root \
     --password financial \
     --database financial \
     --query 삼성전자 \
-    --ds '2024.03.01' \
-    --de '2024.05.20'
+    --ds '2024.05.07' \
+    --de '2024.05.07'
