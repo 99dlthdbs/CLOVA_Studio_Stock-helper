d971b009a90e95b59202df4e286c6ee76ca373dc
JidongKim <apitots@kakao.com>
2024-06-03 14:58:52 +0900

    update

--- jidongkim/routes/auth_routes.py
+++ jidongkim/routes/auth_routes.py
@@ -124,10 +124,7 @@ async def login_for_access_token(
             headers={"WWW-Authenticate": "Bearer"},
         )
 
-    access_token_expires = timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
-    access_token = create_access_token(
-        data={"sub": db_user.email}, expires_delta=access_token_expires
-    )
+    access_token = create_access_token(data={"sub": db_user.email})
     response.set_cookie(
         key="access_token",
         value=f"Bearer {access_token}",
