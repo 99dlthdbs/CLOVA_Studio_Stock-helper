e439d3dc5134fdf312a58a4246f345fd6cf3d6c9
JidongKim <apitots@kakao.com>
2024-06-11 15:27:41 +0900

    deploy

--- jidongkim/main.py
+++ jidongkim/main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import json
 import os
 
-from fastapi import FastAPI, Request, WebSocket, Depends
+from fastapi import FastAPI, Request, WebSocket, Depends, WebSocketDisconnect
 from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -36,7 +36,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 origins = [
     "http://localhost:5173",
     "http://localhost:5174",
-    "http://223.130.140.186:5173",
     "http://223.130.128.222:30002",
 ]
 
@@ -65,87 +64,106 @@ async def web():
 async def websocket_endpoint(
     websocket: WebSocket, db: Session = Depends(get_db_session)
 ):
-    await websocket.accept()
-    while True and websocket.client_state == WebSocketState.CONNECTED:
-        if websocket.client_state != WebSocketState.CONNECTED:
-            break
+    try:
+        await websocket.accept()
+        while True and websocket.client_state == WebSocketState.CONNECTED:
+            try:
+                if websocket.client_state != WebSocketState.CONNECTED:
+                    break
 
-        data = await websocket.receive_text()
+                data = await websocket.receive_text()
 
-        print("data:", data)
+                # msg, token, room_id
+                data = json.loads(data)
 
-        # msg, token, room_id
-        data = json.loads(data)
+                # email, room_id, exp
+                decoded_token = decode_access_token(data["token"])
 
-        # email, room_id, exp
-        decoded_token = decode_access_token(data["token"])
+                db_token = (
+                    db.query(AuthModels.ChatToken)
+                    .filter(AuthModels.ChatToken.token == data["token"])
+                    .first()
+                )
 
-        db_token = (
-            db.query(AuthModels.ChatToken)
-            .filter(AuthModels.ChatToken.token == data["token"])
-            .first()
-        )
+                if not decoded_token or not db_token:
+                    await websocket.send_text("Invalid token")
+                    await websocket.close()
+                    break
 
-        if not decoded_token or not db_token:
-            await websocket.send_text("Invalid token")
-            await websocket.close()
-            break
+                if decoded_token["exp"] < datetime.utcnow().timestamp():
+                    await websocket.send_text("Token expired")
+                    await websocket.close()
+                    break
 
-        if decoded_token["exp"] < datetime.utcnow().timestamp():
-            await websocket.send_text("Token expired")
-            await websocket.close()
-            break
+                room_info = (
+                    db.query(ChattingModels.ChattingRoomModel)
+                    .filter(ChattingModels.ChattingRoomModel.id == data["room_id"])
+                    .first()
+                )
 
-        room_info = (
-            db.query(ChattingModels.ChattingRoomModel)
-            .filter(ChattingModels.ChattingRoomModel.id == data["room_id"])
-            .first()
-        )
+                if not room_info:
+                    await websocket.send_text("Room not found")
+                    await websocket.close()
+                    break
 
-        if not room_info:
-            await websocket.send_text("Room not found")
-            await websocket.close()
-            break
+                user = (
+                    db.query(AuthModels.User)
+                    .filter(AuthModels.User.email == decoded_token["email"])
+                    .first()
+                )
 
-        user = (
-            db.query(AuthModels.User)
-            .filter(AuthModels.User.email == decoded_token["email"])
-            .first()
-        )
+                if room_info.owner_id != user.id:
+                    await websocket.send_text("Not owner of the room")
+                    await websocket.close()
+                    break
 
-        if room_info.owner_id != user.id:
-            await websocket.send_text("Not owner of the room")
-            await websocket.close()
-            break
+                msgs = []
 
-        print("decoded_token:", decoded_token)
+                for item in room_info.chats:
+                    msgs.append(
+                        {
+                            "role": "user",
+                            "content": item.question,
+                        }
+                    )
+                    msgs.append(
+                        {
+                            "role": "assistant",
+                            "content": item.answer,
+                        }
+                    )
 
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[{"role": "user", "content": data["msg"]}],
-            stream=True,
-        )
+                msgs.append(
+                    {
+                        "role": "user",
+                        "content": data["msg"],
+                    }
+                )
 
-        infer_text = ""
+                stream = client.chat.completions.create(
+                    model="gpt-3.5-turbo",
+                    messages=msgs,
+                    stream=True,
+                )
 
-        for chunk in stream:
-            if chunk.choices[0].delta.content is not None:
-                current_content = chunk.choices[0].delta.content
-                infer_text += current_content
+                infer_text = ""
 
-                await websocket.send_text(current_content)
+                for chunk in stream:
+                    if chunk.choices[0].delta.content is not None:
+                        current_content = chunk.choices[0].delta.content
+                        infer_text += current_content
 
-                await asyncio.sleep(0.01)
+                        await websocket.send_text(current_content)
 
-            else:
-                await websocket.close()
-
-        print("str:", infer_text)
-
-        add_chat(
-            room_id=data["room_id"],
-            question=data["msg"],
-            answer=infer_text,
-            db=db,
-            user=user,
-        )
+                        await asyncio.sleep(0.01)
+                add_chat(
+                    room_id=data["room_id"],
+                    question=data["msg"],
+                    answer=infer_text,
+                    db=db,
+                    user=user,
+                )
+            except Exception as e:
+                break
+    except WebSocketDisconnect:
+        websocket.close()
